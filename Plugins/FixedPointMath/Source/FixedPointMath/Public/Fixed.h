6fec1f61fa79fd53280be93075a548d97c8566ff
_Redstone_c_ <2824517378@qq.com>
2020-07-10 20:16:42 +0800

    调整定点数插件文件结构

--- Plugins/FixedPointMath/Source/FixedPointMath/Public/Fixed.h
+++ Plugins/FixedPointMath/Source/FixedPointMath/Public/Fixed.h
@@ -3,6 +3,7 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "FixedPointMath.h"
 #include "Fixed.generated.h"
 
 USTRUCT(BlueprintType)
@@ -19,7 +20,7 @@ struct FIXEDPOINTMATH_API FFixed
 	static constexpr unsigned int DecimalBit = 12;
 
 	FORCEINLINE FFixed() { };
-	FORCEINLINE FFixed(int64 A, bool UseRaw = false) { Data = UseRaw ? A : A << DecimalBit; }
+	FORCEINLINE FFixed(int64 A) { Data = A << DecimalBit; }
 	explicit FORCEINLINE operator int64() const { return Data >> DecimalBit; }
 
 	FORCEINLINE FFixed(int32 A) : FFixed(static_cast<int64>(A)) { }
@@ -87,13 +88,80 @@ struct FIXEDPOINTMATH_API FFixed
 		Data /= RHS.Data;
 		return *this;
 	}
-
 	FORCEINLINE FFixed operator /(const FFixed& RHS) const { FFixed Temp(*this); Temp /= RHS; return Temp; }
 
 	FORCEINLINE FFixed& operator >>=(uint64 RHS) { Data >>= RHS; return *this; }
 	FORCEINLINE FFixed operator >>(uint64 RHS) const { FFixed Temp(*this); Temp >>= RHS; return Temp; }
-
 	FORCEINLINE FFixed& operator <<=(uint64 RHS) { Data <<= RHS; return *this; }
 	FORCEINLINE FFixed operator <<(uint64 RHS) const { FFixed Temp(*this); Temp <<= RHS; return Temp; }
-
 };
+
+namespace FFixedMath
+{
+	// 常用数学
+
+	FORCEINLINE FFixed Abs(FFixed A) { return FMath::Abs(A); }
+	FORCEINLINE FFixed Min(FFixed A, FFixed B) { return FMath::Min(A, B); }
+	FORCEINLINE FFixed Max(FFixed A, FFixed B) { return FMath::Max(A, B); }
+	FORCEINLINE FFixed Clamp(FFixed Value, FFixed Min, FFixed Max) { return FMath::Clamp(Value, Min, Max); }
+	FORCEINLINE FFixed Sqrt(FFixed A);
+
+	// 三角函数
+
+	FIXEDPOINTMATH_API FFixed TanUnit(FFixed A);
+	FIXEDPOINTMATH_API FFixed SinUnit(FFixed A);
+	FIXEDPOINTMATH_API FFixed CosUnit(FFixed A);
+	FORCEINLINE FFixed TanDeg(FFixed A) { return TanUnit(A / FFixed(360)); }
+	FORCEINLINE FFixed SinDeg(FFixed A) { return SinUnit(A / FFixed(360)); }
+	FORCEINLINE FFixed CosDeg(FFixed A) { return CosUnit(A / FFixed(360)); }
+	FORCEINLINE FFixed Tan(FFixed A) { return TanUnit(A / (FFixed(2) * FFixed::Pi)); }
+	FORCEINLINE FFixed Sin(FFixed A) { return SinUnit(A / (FFixed(2) * FFixed::Pi)); }
+	FORCEINLINE FFixed Cos(FFixed A) { return CosUnit(A / (FFixed(2) * FFixed::Pi)); }
+}
+
+FORCEINLINE FFixed FFixedMath::Sqrt(FFixed A)
+{
+	if (A < 0)
+	{
+		UE_LOG(LogFixedPointMath, Warning, TEXT("The square root cannot be negative. (An overflow may have occurred!)"));
+		return 0;
+	}
+
+	// TODO: 将此处的二分求解换成更快的算法
+
+	FFixed Temp;
+
+	if (A == 0) return 0;
+	else
+	{
+		uint64 t;
+		uint64 q = 0;
+		uint64 r = (uint64)A;
+		uint64 b = 0x4000000000000000;
+		while (b > 0)
+		{
+			t = q + b;
+			q >>= 1;
+			if (r >= t)
+			{
+				r -= t;
+				q += b;
+			}
+			b >>= 2;
+		}
+		Temp = FFixed(q);
+
+		FFixed L = Temp;
+		FFixed R = Temp + 1;
+		FFixed M = (L + R) >> 1;
+		while (L < R)
+		{
+			if (M * M < A) L = M + FFixed::Unit;
+			else R = M - FFixed::Unit;
+			M = (L + R) >> 1;
+		}
+		Temp = L;
+	}
+
+	return Temp;
+}
--- Plugins/FixedPointMath/Source/FixedPointMath/Public/FixedVector.h
+++ Plugins/FixedPointMath/Source/FixedPointMath/Public/FixedVector.h
@@ -5,7 +5,7 @@
 #include "CoreMinimal.h"
 
 #include "Fixed.h"
-#include "FixedMath.h"
+#include "FixedPointMath.h"
 #include "FixedVector.generated.h"
 
 USTRUCT(BlueprintType)
@@ -32,23 +32,15 @@ struct FIXEDPOINTMATH_API FFixedVector
 	static const FFixedVector LeftVector;
 
 	FORCEINLINE FFixedVector() { }
-
 	explicit FORCEINLINE FFixedVector(FFixed InF) : X(InF), Y(InF), Z(InF) { }
-
 	FORCEINLINE FFixedVector(FFixed InX, FFixed InY, FFixed InZ) : X(InX), Y(InY), Z(InZ) { }
-
 	explicit FORCEINLINE FFixedVector(const FVector& InVector) : X(InVector.X), Y(InVector.Y), Z(InVector.Z) { }
-
 	explicit FORCEINLINE operator FVector() const { return FVector(static_cast<float>(X), static_cast<float>(Y), static_cast<float>(Z)); }
-
 	explicit FORCEINLINE FFixedVector(const FIntVector& InVector) : X(InVector.X), Y(InVector.Y), Z(InVector.Z) { }
-
 	explicit FORCEINLINE operator FIntVector() const { return FIntVector(static_cast<int32>(X), static_cast<int32>(Y), static_cast<int32>(Z)); }
 
 	FORCEINLINE FFixedVector& operator ^=(const FFixedVector& RHS) { *this = *this ^ RHS; return *this; }
-
 	FORCEINLINE FFixedVector operator ^(const FFixedVector& RHS) const { return FFixedVector(Y * RHS.Z - Z * RHS.Y, Z * RHS.X - X * RHS.Z, X * RHS.Y - Y * RHS.X); }
-
 	FORCEINLINE FFixed operator |(const FFixedVector& RHS) const { return X * RHS.X + Y * RHS.Y + Z * RHS.Z; }
 
 #define FIXED_BIN_OP(O) FORCEINLINE FFixedVector &operator O ##=(const FFixedVector &RHS) { X O##= RHS.X; Y O##= RHS.Y; Z O##= RHS.Z; return *this; } \
@@ -62,20 +54,30 @@ struct FIXEDPOINTMATH_API FFixedVector
 #undef FIXED_BIN_OP
 
 	FORCEINLINE FFixedVector operator -() const { return FFixedVector(-X, -Y, -Z); }
-
 	FORCEINLINE bool operator ==(const FFixedVector& RHS) const { return (X == RHS.X) && (Y == RHS.Y) && (Z == RHS.Z); }
-
 	FORCEINLINE bool operator !=(const FFixedVector& RHS) const { return (X != RHS.X) || (Y != RHS.Y) || (Z != RHS.Z); }
 
 	FORCEINLINE bool IsZero() const { return X == 0 && Y == 0 && Z == 0; }
-
-	FORCEINLINE FFixed Length() const { return FFixedMath::Sqrt(X * X + Y * Y + Z * Z); }
-
+	FORCEINLINE FFixed Length() const { return FFixedMath::Sqrt(LengthSquared()); }
 	FORCEINLINE FFixed LengthSquared() const { return X * X + Y * Y + Z * Z; }
-
 	FORCEINLINE FFixedVector Normalize();
 
 };
+
+namespace FFixedMath
+{
+	// 常用数学
+
+	FORCEINLINE FFixedVector ComponentAbs(const FFixedVector& A) { return FFixedVector(FFixedMath::Abs(A.X), FFixedMath::Abs(A.Y), FFixedMath::Abs(A.Z)); }
+	FORCEINLINE FFixedVector ComponentMin(const FFixedVector& A, const FFixedVector& B) { return FFixedVector(FFixedMath::Min(A.X, B.X), FFixedMath::Min(A.Y, B.Y), FFixedMath::Min(A.Z, B.Z)); }
+	FORCEINLINE FFixedVector ComponentMax(const FFixedVector& A, const FFixedVector& B) { return FFixedVector(FFixedMath::Max(A.X, B.X), FFixedMath::Max(A.Y, B.Y), FFixedMath::Max(A.Z, B.Z)); }
+
+	// 向量运算
+
+	FORCEINLINE FFixedVector CrossProduct(const FFixedVector& A, const FFixedVector& B) { return A ^ B; }
+	FORCEINLINE FFixed DotProduct(const FFixedVector& A, const FFixedVector& B) { return A | B; }
+	FORCEINLINE FFixedVector Normalize(const FFixedVector& A) { FFixedVector Result = A; return Result.Normalize(); }
+}
 
 FORCEINLINE FFixedVector FFixedVector::Normalize()
 {
@@ -88,7 +90,7 @@ FORCEINLINE FFixedVector FFixedVector::Normalize()
 	}
 
 	if (SquareSum > 0)
-		*this /= FFixedVector(Length());
+		*this /= FFixedVector(FFixedMath::Sqrt(SquareSum));
 
 	return *this;
 }
