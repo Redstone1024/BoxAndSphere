f013de52b2bccc6272a942aa231a690b12d0e91e
_Redstone_c_ <2824517378@qq.com>
2020-08-16 17:00:51 +0800

    移除项目对 CheckerAISupport 的模块依赖

--- Source/BoxAndSphere/BoxAndSphere.Build.cs
+++ Source/BoxAndSphere/BoxAndSphere.Build.cs
@@ -8,7 +8,7 @@ public class BoxAndSphere : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "CheckerAISupport" });
+        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay" });
 
     }
 }
